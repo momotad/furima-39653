ebb0c4c392d41b511708db74ba43d0ce09df2a3d
momotad <momota.d@outlook.jp>
2023-08-09 17:10:14 +0900

    パスワードが半角英数字になるように設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email,presence: true, uniqueness: true
-  validates :encrypted_password, presence: true
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers'}
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
