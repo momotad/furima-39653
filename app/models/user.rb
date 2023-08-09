ccfef300e1a3a193849d8b04a5fe6e9e9bca83f2
momotad <momota.d@outlook.jp>
2023-08-09 17:46:16 +0900

    Rubocopでチェック内容を自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -10,18 +9,17 @@ class User < ApplicationRecord
   has_many :buys
 
   validates :nickname, presence: true
-  validates :email,presence: true, uniqueness: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers'}
-  VALID_NAME_REGEX =/\A[ぁ-んァ-ン一-龥々]+\z/
+  validates :email, presence: true, uniqueness: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers' }
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥々]+\z/
 
   with_options presence: true do
-  validates :last_name, format: { with: VALID_NAME_REGEX, message: 'Full-width characters.' }
-  validates :first_name, format: { with: VALID_NAME_REGEX, message: 'Full-width characters.' }
-  validates :last_name_kana, format: { with: VALID_NAME_REGEX, message: 'Full-width characters.' }
-  validates :first_name_kana, format: { with: VALID_NAME_REGEX, message: 'Full-width characters.' }
+    validates :last_name, format: { with: VALID_NAME_REGEX, message: 'Full-width characters.' }
+    validates :first_name, format: { with: VALID_NAME_REGEX, message: 'Full-width characters.' }
+    validates :last_name_kana, format: { with: VALID_NAME_REGEX, message: 'Full-width characters.' }
+    validates :first_name_kana, format: { with: VALID_NAME_REGEX, message: 'Full-width characters.' }
   end
 
   validates :birthday, presence: true
-
 end
