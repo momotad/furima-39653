bd90559f8a6cc961a7b1e3911b0d27abdbc4c70c
momotad <momota.d@outlook.jp>
2023-08-09 18:21:44 +0900

    アソシエーション削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :buys
-
   validates :nickname, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers' }
