5f25ba2789fc1f862850695474cc63528842eb5b
momotad <momota.d@outlook.jp>
2023-08-10 20:07:45 +0900

    index.jsのconsole.logの記述を削除

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,12 +1,9 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
- //  console.log(priceInput)
    priceInput.addEventListener("input", () => {
      const TaxForm = document.getElementById("add-tax-price");
-     // console.log(priceInput.innerHTML);
      TaxForm.innerHTML = Math.round(priceInput.value * 0.1 );
      const addProfitDom = document.getElementById("profit");
-     // console.log(priceInput.innerHTML);
      addProfitDom.innerHTML = Math.round(priceInput.value - TaxForm.innerHTML)
  })
 });
