60b801b3937cec2add6a4555bf1ddaf549485984
momotad <momota.d@outlook.jp>
2023-08-10 11:50:53 +0900

    itemモデルの記述内容修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,25 @@
 class Item < ApplicationRecord
 
   belongs_to :user
-  belongs_to :category
-  belongs_to :status
-  belongs_to :cost
-  belongs_to :area
-  belongs_to :day
   # has_one :buy
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :status
+    belongs_to :cost
+    belongs_to :area
+    belongs_to :day
+  end
 
   validates :user, presence: true
   validates :name, presence: true
   validates :content, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :cost_id, presence: true
-  validates :area_id, presence: true
-  validates :day_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :day_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true
 
 end
