bfb4a49e9070d976db6580cabbf47b1ec1df1ffe
momotad <momota.d@outlook.jp>
2023-08-10 12:14:58 +0900

    new,createアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+private
+  def item_params
+    params.require(:item).permit( :image, :name, :content, :category_id, :status_id, :cost_id, :area_id, :day_id, :price ).merge(user_id: current_user.id)
+  end
+
+
+
 end
