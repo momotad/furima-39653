cc9137e24b69a75d61780a29b86c81e9f6e8fbd5
momotad <momota.d@outlook.jp>
2023-08-10 12:02:31 +0900

    マイグレーションファイル修正

--- db/migrate/20230810022857_create_items.rb
+++ db/migrate/20230810022857_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.references :user,    null: false, default: "", foreign_key: true
-      t.string :name,        null: false, default: ""
-      t.text :content,       null: false, default: ""
-      t.integer :category_id null: false, default: "---"
-      t.integer :status_id   null: false, default: "---"
-      t.integer :cost_id     null: false, default: "---"
-      t.integer :area_id     null: false, default: "---"
-      t.integer :day_id      null: false, default: "---"
-      t.integer :price,      null: false, default: ""
+      t.references :user,     null: false, foreign_key: true
+      t.string :name,         null: false, default: ""
+      t.text :content,        null: false
+      t.integer :category_id, null: false
+      t.integer :status_id,   null: false
+      t.integer :cost_id,     null: false
+      t.integer :area_id,     null: false
+      t.integer :day_id,      null: false
+      t.integer :price,       null: false, default: ""
       t.timestamps
     end
   end
