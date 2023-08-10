ed8972375bc820edcee05a708090b9d5335d5310
momotad <momota.d@outlook.jp>
2023-08-10 20:16:18 +0900

    index.jsの切り捨て計算をMath.floorに変更

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -2,8 +2,8 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
    priceInput.addEventListener("input", () => {
      const TaxForm = document.getElementById("add-tax-price");
-     TaxForm.innerHTML = Math.round(priceInput.value * 0.1 );
+     TaxForm.innerHTML = Math.floor(priceInput.value * 0.1 );
      const addProfitDom = document.getElementById("profit");
-     addProfitDom.innerHTML = Math.round(priceInput.value - TaxForm.innerHTML)
+     addProfitDom.innerHTML = Math.floor(priceInput.value - TaxForm.innerHTML)
  })
 });
