89dbe9c90b12fe8a1af4e7fb21a2c0e4f6f76311
momotad <momota.d@outlook.jp>
2023-08-13 15:46:26 +0900

    Buyモデル作成に伴うアソシエーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   # has_one :buy
   has_one_attached :image
+  has_one :buy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
