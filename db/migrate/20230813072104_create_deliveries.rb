86b9b8612c0b8bbf6df6402e0db3c6ea87a02a9a
momotad <momota.d@outlook.jp>
2023-08-13 16:26:21 +0900

    Deliveryモデルのマイグレーションファイル作成

--- db/migrate/20230813072104_create_deliveries.rb
+++ db/migrate/20230813072104_create_deliveries.rb
@@ -1,7 +1,13 @@
 class CreateDeliveries < ActiveRecord::Migration[7.0]
   def change
     create_table :deliveries do |t|
-
+      t.references :buy,     null: false, foreign_key: true
+      t.string :postcode,     null: false
+      t.integer :area_id,     null: false
+      t.string :city,         null: false
+      t.string :address,      null: false
+      t.string :building
+      t.string :telephone,    null: false
       t.timestamps
     end
   end
