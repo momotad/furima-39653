85918e2483911df77db022686da6aa4ce8c203b7
momotad <momota.d@outlook.jp>
2023-08-15 11:57:36 +0900

    formオブジェクト作成に伴うDeliveryモデルのバリデーション削除

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -4,10 +4,11 @@ class Delivery < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :area
 
-  validates :postcode, presence: true, format: { with: ^[0-9]{3}-[0-9]{4}$, message: 'Enter it as follows (e.g. 123-4567)' }
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :city, presence: true
-  validates :address, presence: true
-  validates :telephone, presence: true, format: { with: ^0[789]0[\d]{7,8}$, message: 'Input only number' }
+  # 以下バリデーションはFromオブジェクトのBuyDeliveryに転記済み
+  # validates :postcode, presence: true, format: { with: ^[0-9]{3}-[0-9]{4}$, message: 'Enter it as follows (e.g. 123-4567)' }
+  # validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+  # validates :city, presence: true
+  # validates :address, presence: true
+  # validates :telephone, presence: true, format: { with: ^0[789]0[\d]{7,8}$, message: 'Input only number' }
 
 end
