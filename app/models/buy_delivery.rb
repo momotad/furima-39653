6c603fd1c6880787979450a36b18c03b82eaef07
momotad <momota.d@outlook.jp>
2023-08-15 21:44:01 +0900

    formオブジェクトのbuy_deliveryを修正

--- app/models/buy_delivery.rb
+++ app/models/buy_delivery.rb
@@ -1,6 +1,6 @@
 class BuyDelivery
   include ActiveModel::Model
-  attr_accessor :postcode, :area_id, :city, :address, :building, :telephone, :user_id, :item_id, :buy_id
+  attr_accessor :postcode, :area_id, :city, :address, :building, :telephone, :user_id, :item_id, :buy_id, :token
 
   with_options presence: true do
     validates :postcode, format: { with: /\A^[0-9]{3}-[0-9]{4}$+\z/, message: 'Enter it as follows (e.g. 123-4567)' }
@@ -9,11 +9,12 @@ class BuyDelivery
     validates :telephone, format: { with: /\A^0[789]0[\d]{7,8}$+\z/, message: 'Input only number' }
     validates :user_id
     validates :item_id
+    validates :token
   end
     validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
-    buy = Buy.create(user_id: user_id, item_id: item_id)
+    buy = Buy.create(user_id: user_id, item_id: item_id.id)
     Delivery.create(postcode: postcode, area_id: area_id, city: city, address: address, building: building, telephone: telephone, buy_id: buy.id)
   end
 
