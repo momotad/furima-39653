b7c30c5db156f74c8919e1822da2cc29ede971a7
momotad <momota.d@outlook.jp>
2023-08-15 11:59:31 +0900

    各アクションの設定変更

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,16 +1,28 @@
 class BuysController < ApplicationController
 
   def index
-    @buy = Buy.new
+    @buy_delivery = BuyDelivery.new
     @item = Item.find(params[:item_id])
   end
 
   def new
-
+    @buy_delivery = BuyDelivery.new
   end
 
   def create
+    # binding.pry
+    @buy_delivery = BuyDelivery.new(buy_params)
+    if @buy_delivery.valid?
+      @buy_delivery.save
+      return redirect_to '/'
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
 
+private
+  def buy_params
+    params.require(:buy_delivery).permit(:item_id, :postcode, :area_id, :city, :address, :building, :telephone).merge(user_id: current_user.id).merge(token: params[:token])
   end
 
 end
