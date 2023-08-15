8b72a5bf0dab614996c074faeff13a15c550f454
momotad <momota.d@outlook.jp>
2023-08-15 11:57:08 +0900

    formオブジェクトの作成

---
+++ app/models/buy_delivery.rb
@@ -0,0 +1,20 @@
+class BuyDelivery
+  include ActiveModel::Model
+  attr_accessor :postcode, :area_id, :city, :address, :building, :telephone, :user_id, :item_id, :buy_id
+
+  with_options presence: true do
+    validates :postcode, format: { with: /\A^[0-9]{3}-[0-9]{4}$+\z/, message: 'Enter it as follows (e.g. 123-4567)' }
+    validates :city
+    validates :address
+    validates :telephone, format: { with: /\A^0[789]0[\d]{7,8}$+\z/, message: 'Input only number' }
+    validates :user_id
+    validates :item_id
+  end
+    validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  def save
+    buy = Buy.create(user_id: user_id, item_id: item_id)
+    Delivery.create(postcode: postcode, area_id: area_id, city: city, address: address, building: building, telephone: telephone, buy_id: buy.id)
+  end
+
+end
